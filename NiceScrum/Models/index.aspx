d391f1786385cc2a8b809e3a345bcd1291e38bcf
Dhrumil <DhrumilPa.95@Gmail.Com>
2016-09-16 22:17:26 -0500

    Removed .gitignore and .gitattributes.

---
+++ NiceScrum/Models/index.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="NiceScrum.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Page</title>
+    <style type="text/css">
+        #Text1 {
+            width: 224px;
+            height: 18px;
+        }
+        #Text2 {
+            width: 224px;
+            height: 18px;
+        }
+    </style>
+</head>
+<body style ="background: #F0F8FF">
+    <center>
+    <form id="form1" runat="server" align ="middle">
+    <h1>
+        Login Page</h1>
+    <p>
+        &nbsp;</p>
+    <p  style="font-size:25px">
+        &nbsp;&nbsp; Username&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server" Height="18px"  Width="224px"></asp:TextBox>
+        </p>
+        <p  style="font-size:25px">
+        &nbsp;&nbsp; Password&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server" Height="18px" Width="224px"></asp:TextBox>
+        </p>
+        <p style="font-size:25px">
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" Width="121px" />
+        </p>
+    </form>
+        </center>
+    </body>
+</html>
---
+++ NiceScrum/Models/index.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+
+namespace NiceScrum
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Button1.Text = "Submit";
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string filename, version, connectioninfo;
+            SqlConnection db;
+
+            version = "MSSQLLocalDB";
+            filename = "NiceScrumDB.mdf";
+
+            connectioninfo = String.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\NiceScrumDB.mdf;Integrated Security=True");
+
+            db = new SqlConnection(connectioninfo);
+            db.Open();
+
+            string username = TextBox1.Text;
+            string password = TextBox2.Text;
+
+            Console.Write(username);
+            Console.WriteLine(password);
+            string sql = String.Format(@"select name, accType, userid from users where username = '{0}' and pass= '{1}'",username,password);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = db;
+            cmd.CommandText = sql;
+            object result = new object();
+            result = cmd.ExecuteScalar();
+         
+
+            db.Close();
+            string userName = "";
+            if (result == null) Console.Write("Item was not found"); // debug
+            else if (result == DBNull.Value)
+                Console.Write("Item found but value was NULL"); // debug
+            else
+            {
+                userName = Convert.ToString(result);
+            }
+
+            Session.Add("test", userName);
+            
+            Response.Redirect("homepage.aspx");
+        }
+    }
+}
